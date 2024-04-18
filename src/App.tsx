550ac2797ac4d8f55c88850fbb7ad65eb19aea01
jullysilva <jkasilva@sga.pucminas.br>
2024-04-18 14:38:57 -0300

    [CODE]: Re-created project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import HomeBroker from './HomeBroker';
+
+function App() {
+  return (
+    <div className="App">
+      <HomeBroker />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/HomeBroker.styled.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+import { Box } from '@mui/material/';
+
+
+export const BoxHB = styled(Box)`
+    color: white;
+    background-color: black;
+`;
