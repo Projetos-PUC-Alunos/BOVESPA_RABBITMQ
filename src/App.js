ac31608c07be628c5bdb6da512b95c3ac98768a7
jullysilva <jkasilva@sga.pucminas.br>
2024-05-02 23:00:44 -0300

    atualizando list de transacoes

--- src/App.js
+++ src/App.js
@@ -8,9 +8,17 @@ function App() {
   const [transacoes, setTransacoes] = useState([]);
 
   useEffect(() => {
-    const response = apiConsumer.get("/transacoes");
-    console.log(response);
-  }, []);
+    const fetchData = async () => {
+      try {
+        const response = await apiConsumer.get("/transacoes");
+        setTransacoes(response.data);
+      } catch (error) {
+        console.error("Erro ao processar solicitação:", error);
+      }
+    };
+  
+    fetchData();
+  }, [transacoes]);
 
 
   const { register, handleSubmit } = useForm({});
@@ -24,8 +32,7 @@ function App() {
   
     try {
       if (formData.tipoEvento === "compra") {
-        const reponse = await apiBroker.post("/compra", data);
-        console.log("Compra realizada com sucesso!", reponse);
+        await apiBroker.post("/compra", data);
       } else if (formData.tipoEvento === "venda") {
         const reponse = await apiBroker.post("/venda", data);
 
@@ -39,12 +46,15 @@ function App() {
 
 
   return (
-    <div class="container">
-        <div class="row row-cols-2 g-4">
-            <div class="col border bg-light">
+    <div class="container-fluid bg-dark">
+      <div>
+        <p class="fs-1 text-white text-center">Homebroker</p>
+      </div>
+        <div class="row row-cols-2 gy-5 justify-content-around">
+            <div class="col-3 bg-light p-4">
                 <form onSubmit={handleSubmit(handleSolicitation)}>
                     <div class="mb-3">
-                      <label for="exampleInputEmail1" class="form-label">Titulo Ação</label>
+                      <label class="form-label">Titulo Ação</label>
                       <select className="form-select" aria-label="Default select example" {...register('acao')}>
                         {acoes.map((index) => (
                           <option key={index} value={index} selected>{index}</option>
@@ -53,58 +63,63 @@ function App() {
                     </div>
 
                     <div class="mb-3">
-                        <label for="exampleInputPassword1" class="form-label">Quantidade</label>
-                        <input type="number" class="form-control" id="exampleInputPassword1" {...register('quantAcoes')} />
+                        <label class="form-label">Quantidade</label>
+                        <input type="number" class="form-control" {...register('quantAcoes')} />
                     </div>
                     <div class="mb-3">
-                        <label for="exampleInputPassword1" class="form-label">Valor</label>
-                        <input type="text" class="form-control" id="exampleInputPassword1" {...register('valor')}/>
+                        <label class="form-label">Valor</label>
+                        <input type="text" class="form-control" {...register('valor')}/>
                     </div>
                     <div className="form-check form-check-inline">
                       <input
                         className="form-check-input"
                         type="radio"
-                        id="inlineRadio1"
                         value="compra"
-                        {...register('tipoEvento', { required: true })} // Registrar o campo 'tipoEvento' e adicionar validação (opcional)
+                        {...register('tipoEvento', { required: true })}
                       />
-                      <label className="form-check-label" htmlFor="inlineRadio1">Compra</label>
+                      <label className="form-check-label">Compra</label>
                     </div>
                     <div className="form-check form-check-inline">
                       <input
                         className="form-check-input"
                         type="radio"
-                        id="inlineRadio2"
                         value="venda"
-                        {...register('tipoEvento', { required: true })} // Registrar o campo 'tipoEvento' e adicionar validação (opcional)
+                        {...register('tipoEvento', { required: true })}
                       />
-                      <label className="form-check-label" htmlFor="inlineRadio2">Venda</label>
+                      <label className="form-check-label">Venda</label>
                     </div>
-                    <button type="submit" class="btn btn-primary d-block">solicitar</button>
+                    <button type="submit" class="btn btn-primary float-end mt-2 d-block">solicitar</button>
                 </form>
             </div>
-            <div class="col border bg-light">
+            <div class="col-8 bg-light ms-1">
                 <table class="table">
                     <thead>
                     <tr>
                         <th scope="col">#</th>
-                        <th scope="col">solicitacaoVenda</th>
-                        <th scope="col">solicitacaoCompra</th>
+                        <th scope="col">Ativo</th>
+                        <th scope="col">Corretora</th>
+                        <th scope="col">Valor</th>
+                        <th scope="col">Quantidade</th>
                         <th scope="col">dataTransacao</th>
                     </tr>
                     </thead>
                     <tbody>
-                    <tr>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                    </tbody>
+                    {transacoes?.map((row, index) => (
+                      <tr key={index}>
+                        <td>{row.index}</td>
+                        <td>{row.solicitacaoCompra ? row.solicitacaoCompra.ativo : ""}</td>
+                        <td>{row.solicitacaoCompra ? row.solicitacaoCompra.corretora : ""}</td>
+                        <td>{row.solicitacaoCompra ? row.solicitacaoCompra.real : ""}</td>
+                        <td>{row.solicitacaoCompra ? row.solicitacaoCompra.quant : ""}</td>
+                        <td>{row.dataTransacao}</td>
+                      </tr>
+                    ))}
+                  </tbody>
                 </table>
             </div>
-            <div class="col border bg-light">Column</div>
-            <div class="col border bg-light">Column</div>
-        </div>
+            <div class="col bg-light">Column</div>
+            <div class="col bg-light">Column</div>
+          </div>
     </div>
   );
 }
